801616e29fbecb26b47ef323d8b4b291b8ce8a9d
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-05-25 12:22:14 +0900

    拍手された数の実装

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,8 +6,42 @@ class Introduction extends React.Component {
         super(props);
         this.state = {
             userInfo_user: this.props.userInfo_user,
-            post_user: this.props.post_user
+            post_user: this.props.post_user,
+            count: 0
         };
+    }
+
+    handleClick() {
+        
+        // 拍手した人が紹介した人・紹介された人でなければ
+        if (!(this.props.userInfo_user.name == this.state.userInfo_user.name || this.props.userInfo_user.name == this.state.post_user.name)) {
+            this.setState({
+                count: this.state.count + 1,
+            });
+
+            // 紹介した人の拍手された数が+1される
+            if (this.state.userInfo_user.name == this.props.user_01.name) {
+                this.props.user_01.applauded += 1;
+            }
+            if (this.state.userInfo_user.name == this.props.user_02.name) {
+                this.props.user_02.applauded += 1;
+            }
+            if (this.state.userInfo_user.name == this.props.user_03.name) {
+                this.props.user_03.applauded += 1;
+            }
+
+            // 紹介された人の拍手された数が+1される
+            if (this.state.post_user.name == this.props.user_01.name) {
+                this.props.user_01.applauded += 1;
+            }
+            if (this.state.post_user.name == this.props.user_02.name) {
+                this.props.user_02.applauded += 1;
+            }
+            if (this.state.post_user.name == this.props.user_03.name) {
+                this.props.user_03.applauded += 1;
+            }
+        }
+
     }
 
     render() {
@@ -22,8 +56,8 @@ class Introduction extends React.Component {
                     <p>{this.props.message}</p>
                     <div className="d-flex flex-row justify-content-between">
                         <div className="">
-                            <i className="fas fa-sign-language fa-2x mr-3"></i>
-                            <span>0</span>
+                            <i className="fas fa-sign-language fa-2x mr-3" onClick = {()=>{this.handleClick()}}></i>
+                            <span>{this.state.count}</span>
                         </div>
                         <p>{this.props.date}</p>
                     </div>
